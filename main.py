d271699f408b0c6562f2937cac64ee4762f5015d
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-09-30 18:24:05 +0530

    Disabled button

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -10,7 +10,7 @@ from kivy.app import App
 
 class WidgetExample (GridLayout):
     count = 1
-    count_enabled = False
+    count_enabled = BooleanProperty(False) 
     my_text = StringProperty("1") 
     def on_button_click (self):
         print("Button Cliked")
