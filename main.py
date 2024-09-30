eb6cb9bafa8ef42df003ae82ee1b53d7f26f2519
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-09-30 14:03:38 +0530

    Button click

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.app import App 
+
+class WidgetExample (GridLayout):
+    my_text = StringProperty("YARA!") 
+    def on_button_click (self):
+        print("Button Cliked")
+        self.my_text = "ASTRO"
 
 class StackLayoutExample (StackLayout): 
     def __init__(self, **kwargs):
